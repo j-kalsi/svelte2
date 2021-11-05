3da2f759df5e173159375c04d5f8041030e0ed1f
Jasnoor Kalsi <87153709+j-kalsi@users.noreply.github.com>
2021-11-06 01:19:53 +0530

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,38 @@
+<script>
+	let firstName  = "";
+	let lastName = "";
+    let src = "download.png";
+	let count= 0;
+	$: fullName =`${firstName} ${lastName}`
+	$: {
+		console.log(firstName);
+		console.log(lastName);
+	}
+   const resetName = () =>{
+	   firstName= "unknown";
+	   lastName= "unknown";
+   }
+</script>
+
+<main>
+	<h1>hello {fullName}</h1>
+	<label for="firstName">First Name</label>
+    <input type="text" bind:value={firstName}>
+	<label for="lastName">Last Name</label>
+	<input type="text" bind:value={lastName}>
+	<button on:click={resetName}>set name to unknown</button>
+	<img src="src" alt="svelte logo">
+</main>
+
+<style>
+	main{
+			display: flex;
+	flex-direction: column;
+	align-items: center;
+	}
+	h1{
+		font-size: 72px;
+		text-align: center;
+	}
+	
+</style>
